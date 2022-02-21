bd955b87ff8279b075929e96c0cfdbfb4bf86af3
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-20 23:12:04 -0500

    added logic to database

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -23,9 +23,7 @@ module.exports = () => {
         template: "./index.html",
         title: "J.A.T.E"
       }),
-
-      new WorkboxPlugin.GenerateSW(),
-
+    
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
