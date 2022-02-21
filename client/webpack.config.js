162ca404de623272cfdf31121dcd5b2ab652a063
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-20 19:24:48 -0500

    added scripts to root package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -23,6 +24,8 @@ module.exports = () => {
         title: "J.A.T.E"
       }),
 
+      new WorkboxPlugin.GenerateSW(),
+
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
@@ -42,6 +45,16 @@ module.exports = () => {
         { 
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
         },
       ],
     },
